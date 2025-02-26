4e0a962a0ac183354fb105b8ea7efee8164494c3
Shreesat Sahu <shreesat.sahu@iigtechnology.in>
2025-02-26 18:17:51 +0530

    ADD: Integrate emailjs-com for form submission in contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,6 +4,7 @@ import Head from "next/head";
 import AnimatedText from "@/components/AnimatedText";
 import TransitionEffect from "@/components/TransitionEffect";
 import { useRouter } from "next/router";
+import emailjs from "emailjs-com";
 
 // Netlify Form config
 
@@ -21,47 +22,34 @@ export default function About() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const myForm = e.target;
-    const formData = new FormData(myForm);
-    try {
-      const response = await fetch("/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams(formData).toString(),
-      });
 
-      if (response.ok) {
-        // Handle success, e.g., redirect to a thank you page
+    try {
+      const result = await emailjs.sendForm("service_nv1hmhm", "template_ku5wwrv", e.target, "lY7PlQB2acvsd1ZSf");
+
+      if (result.text === "OK") {
+        console.log("Email sent successfully:", result);
         router.push("/thanks");
       } else {
-        // Handle error
-        console.error("Form submission failed!", response);
+        console.error("Email sending failed!", result);
       }
     } catch (error) {
-      // Handle error
-      console.error("An error occurred during form submission:", error);
+      console.error("An error occurred while sending the email:", error);
     }
   };
 
   return (
     <>
       <Head>
-        <title>ShreesatPortfolio Built with Nextjs</title>
+        <title>Shreesat Sahu's Portfolio</title>
         <meta name="description" content="ShreesatPortfolio, A open-source portfolio theme built with Nextjs" />
       </Head>
 
       <TransitionEffect />
       <main className={`flex w-full flex-col items-center justify-center dark:text-light`}>
         <Layout className="pt-16">
-          <AnimatedText
-            text="Begin Today,
-I'm One Message Away 👋"
-            className="mb-16 !text-8xl !leading-tight lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8"
-          />
+          <AnimatedText text="Begin Today, I'm One Message Away 👋" className="mb-16 !text-8xl !leading-tight lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8" />
 
-          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8 relative flex w-full flex-col items-center justify-center rounded-2xl rounded-br-2xl border border-solid border-dark bg-light p-6 shadow-2xl dark:border-light dark:bg-dark xs:p-4">
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8 relative flex flex-col items-center justify-center rounded-2xl rounded-br-2xl border border-solid border-dark bg-light p-6 shadow-2xl dark:border-light dark:bg-dark xs:p-4">
             <div className="absolute top-0 -right-5 -z-10 h-[103%] w-[101.5%] rounded-[2rem] rounded-br-3xl bg-dark dark:bg-light md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]" />
             <div className="col-span-4 flex flex-col items-start justify-start xl:col-span-4 md:order-1 md:col-span-8">
               <h2 className="my-4 text-2xl font-bold capitalize text-primaryDark dark:text-primaryDark">What’s Next?</h2>
@@ -70,7 +58,7 @@ I'm One Message Away 👋"
               <p className="">My inbox is always open. Whether you have a question or just want to say hello, I'll try my best to get back to you! Feel free to message me about any relevant project updates.</p>
             </div>
             <div className="relative col-span-4 h-max xl:col-span-4 md:col-span-8 md:order-2">
-              <div className="grid w-full grid-cols-2 sm:gap-6 relative flex w-full flex-col items-center justify-center rounded-2xl rounded-br-2xl border  border-solid  border-dark bg-light p-6   dark:border-light dark:bg-dark xs:p-4">
+              <div className="grid w-full grid-cols-2 sm:gap-6 relative flex flex-col items-center justify-center rounded-2xl rounded-br-2xl border  border-solid  border-dark bg-light p-6   dark:border-light dark:bg-dark xs:p-4">
                 <div className="col-span-8 h-max xl:col-span-6 md:col-span-8 md:order-2">
                   <form name="contact-form" method="POST" onSubmit={handleSubmit}>
                     <input type="hidden" name="form-name" value="contact-form" />
@@ -83,14 +71,14 @@ I'm One Message Away 👋"
                     <div className="col-span-1 p-2">
                       <label className="block text-sm font-medium text-dark dark:text-light">
                         Your Name:
-                        <input type="text" name="name" required autoComplete="name" className="mt-1 p-2 w-full border border-solid border-dark rounded-md bg-light dark:border-light dark:bg-dark dark:text-light" onChange={handleChange} />
+                        <input type="text" name="to_name" required autoComplete="name" className="mt-1 p-2 w-full border border-solid border-dark rounded-md bg-light dark:border-light dark:bg-dark dark:text-light" onChange={handleChange} />
                       </label>
                     </div>
 
                     <div className="col-span-1 p-2">
                       <label className="block text-sm font-medium text-dark/75 dark:text-light/75">
                         Your Email:
-                        <input type="email" name="email" required autoComplete="off" className="mt-1 p-2 w-full border border-solid border-dark rounded-md bg-light dark:border-light dark:bg-dark dark:text-light" onChange={handleChange} />
+                        <input type="email" name="from_name" required autoComplete="off" className="mt-1 p-2 w-full border border-solid border-dark rounded-md bg-light dark:border-light dark:bg-dark dark:text-light" onChange={handleChange} />
                       </label>
                     </div>
 
